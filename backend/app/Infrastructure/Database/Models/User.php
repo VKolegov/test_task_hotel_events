81de7b7f23a290e7772484a7bbd3bc65c89c5c0e
Vladislav Kolegov <dev@vkolegov.ru>
2025-02-21 12:29:09 +0700

    ide helper (php doc generator)

--- backend/app/Infrastructure/Database/Models/User.php
+++ backend/app/Infrastructure/Database/Models/User.php
@@ -7,7 +7,23 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property int|null $role_id
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
--- backend/app/Infrastructure/Database/Models/UserRole.php
+++ backend/app/Infrastructure/Database/Models/UserRole.php
@@ -4,7 +4,25 @@ namespace App\Infrastructure\Database\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property string[] $permissions
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ */
 class UserRole extends Model
 {
     protected $table = 'user_roles';
+
+    protected function casts(): array
+    {
+        return [
+            'permissions' => 'array'
+        ];
+    }
 }

59fc35cd9c94173aaa93f5c3629b01fb3be0de00
Vladislav Kolegov <dev@vkolegov.ru>
2025-02-25 11:50:38 +0700

    fix booking factory

--- backend/app/Infrastructure/Laravel/database/factories/BookingFactory.php
+++ backend/app/Infrastructure/Laravel/database/factories/BookingFactory.php
@@ -12,12 +12,15 @@ class BookingFactory extends Factory
 
     public function definition()
     {
-        $checkOut = \Carbon\CarbonImmutable::parse($this->faker->dateTimeBetween('-3 months', 'now'));
+        $createdAt = \Carbon\CarbonImmutable::parse($this->faker->dateTimeBetween('-3 months', '-2 months'));
+
+        $checkOut = \Carbon\CarbonImmutable::parse($this->faker->dateTimeBetween('-10 days', '+14 days'));
         $checkIn = $checkOut->subDays(
             $this->faker->numberBetween(1, 14)
         );
 
         return [
+            'created_at' => $createdAt,
             'check_in' => $checkIn->format('Y-m-d'),
             'check_out' => $checkOut->format('Y-m-d'),
             'price' => $this->faker->randomFloat(2, 10000, 3000000),
